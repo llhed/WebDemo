7ecd3ba81322a14098d4ab456e63648ffe8068d1
lim <llhed_lm@hotmail.com>
2015-08-28 14:41:42 +0800

    完整版

---
+++ src/main/java/me/lim/controller/UserController.java
@@ -0,0 +1,28 @@
+package me.lim.controller;
+
+import me.lim.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created by lim on 2015/8/28.
+ */
+
+@Controller
+public class UserController {
+
+
+    @RequestMapping("/example")
+    public String example(ModelMap modelMap,HttpServletRequest request){
+
+        User usr = new User();
+        usr.setNickname("lim");
+
+        modelMap.put("usr",usr);
+
+        return "example";
+    }
+}
